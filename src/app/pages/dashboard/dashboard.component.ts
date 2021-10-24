c23e8aeca521ba1a8c40497db27fb8d5a71c6d83
Catalina Cano <catalinacano08@hotmail.com>
2021-10-23 19:30:29 -0500

    Cambios Menores

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -39,9 +39,6 @@ export class DashboardComponent {
         height: 350,
         type: "bar"
       },
-      title: {
-        text: "My First Angular Chart"
-      },
       xaxis: {
         categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
       }
