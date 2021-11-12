800340eb4e8d3ba46d5a6b05f518910a696986b0
Catalina Cano Narvaez <catalina.cano@realpage.com>
2021-11-11 21:46:00 -0500

    Version final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url_base: 'https://demozeus.azurewebsites.net/api/UploadFile?code=Skgajgfla3vJFM/pFSnskeU0wCGqY0Rsh57aEzmCn944tqMt5a/qrA==',
+  url_vision:'https://eastus2.api.cognitive.microsoft.com/vision/v3.2/read/analyze',
+  Ocp_Apim:'69595de4520f490b88f54af3dcdb3909'
 };
