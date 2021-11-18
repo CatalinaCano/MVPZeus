ef3262a0b6993d4f975c9957d22b4b38d75cdf36
Catalina Cano <catalinacano08@hotmail.com>
2021-11-17 21:20:30 -0500

    Funcionando el Sidebar toogle

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,26 @@ export class HeaderComponent implements OnInit {
 
   date = new Date().toDateString();
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+  ) { }
 
   ngOnInit(): void {
+
   }
+
+  
+
+
+  closeMenu() {
+
+    if (!this.document.body.classList.contains('toggle-sidebar')) {
+      this.renderer.addClass(this.document.body, 'toggle-sidebar');
+    } else {
+      this.renderer.removeClass(this.document.body, 'toggle-sidebar');
+    }
+  }
+
 
 }
